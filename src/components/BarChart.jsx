c2fb05f3982183ce58d2973b4b067920f1db4974
samvat-tiwari <samvat@passprt.co>
2024-11-11 00:13:22 +0530

    initial commit

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -26,10 +26,179 @@ const BarChart = ({ isDashboard = false, isBarVsYear, propData, country }) => {
   
     console.log("[Bar Vs Year]data being rendered: ", finalRenderingData);
     console.log("isBarVsYear: ", isBarVsYear);
+    
+// {
+//     // set finalRenderingData to the below data 
+//     // 0
+//     // : 
+//     // {country: 'United States of America', Retail: 309, Manufacturing: 210, Water: 146, Energy: 940, …}
+//     // 1
+//     // : 
+//     // {country: 'Russia', Energy: 176, Aerospace & defence: 32, Financial services: 56, Manufacturing: 12, …}
+//     // 2
+//     // : 
+//     // {country: 'Saudi Arabia', Energy: 298, Tourism & hospitality: 45, Support services: 6, Retail: 12, …}
+//     // 3
+//     // : 
+//     // {country: 'India', Transport: 32, Water: 16, Energy: 248, Financial services: 32, …}
+//     // 4
+//     // : 
+//     // {country: 'Iran', Energy: 90, Tourism & hospitality: 32, Retail: 36, Financial services: 150, …}
+    
+//     // finalRenderingData = [
+//     //   {
+//     //     "country": "T:21",
+//     //     // use js library to calculate logarithm of the value
+//     //     "Energy": (1/573)*10000,
+        
+//     //     "id": "2018"
+//     //   },
+//     //   {
+//     //     "country": "Russia",
+//     //     "Energy": (1/1000)*10000,
+    
+//     //     "id": "2018"
+//     //   },
+//     //   {
+//     //     "country": "Saudi Arabia",
+//     //     "Energy": (1/10000)*10000,
+       
+//     //     "id": "2018"
+//     //   },
+//     // ];
   
+//     // finalRenderingData.map((data) => {
+//     //   data["country"] = `1:${(data["Energy"]*100).toFixed()}`;
+//     // });
+//     }
 
-  
   return (
+
+//     <ResponsiveBar
+//   data={finalRenderingData}
+//   theme={{
+//     axis: {
+//       domain: {
+//         line: {
+//           stroke: colors.grey[100],
+//         },
+//       },
+//       legend: {
+//         text: {
+//           fill: colors.grey[100],
+//         },
+//       },
+//       ticks: {
+//         line: {
+//           stroke: colors.grey[100],
+//           strokeWidth: 1,
+//         },
+//         text: {
+//           fill: colors.grey[100],
+//         },
+//       },
+//     },
+//     legends: {
+//       text: {
+//         fill: colors.grey[100],
+//       },
+//     },
+//   }}
+//   keys={[
+//     "Energy",
+//     "Manufacturing",
+//     "Agriculture",
+//     "Transport",
+//     "Residential",
+//     "Financial services",
+//   ]}
+//   indexBy={isBarVsYear ? "id" : "country"}
+//   margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+//   padding={0.3}
+//   valueScale={{ type: "linear" }}
+//   indexScale={{ type: "band", round: true }}
+//   colors={{ scheme: "nivo" }}
+//   defs={[
+//     {
+//       id: "dots",
+//       type: "patternDots",
+//       background: "inherit",
+//       color: "#38bcb2",
+//       size: 4,
+//       padding: 1,
+//       stagger: true,
+//     },
+//     {
+//       id: "lines",
+//       type: "patternLines",
+//       background: "inherit",
+//       color: "#eed312",
+//       rotation: -45,
+//       lineWidth: 6,
+//       spacing: 10,
+//     },
+//   ]}
+//   borderColor={{
+//     from: "color",
+//     modifiers: [["darker", "1.6"]],
+//   }}
+//   axisTop={null}
+//   axisRight={null}
+//   axisBottom={{
+//     tickSize: 5,
+//     tickPadding: 5,
+//     tickRotation: 0,
+//     legend: isBarVsYear ? "id" : "country",
+//     legendPosition: "middle",
+//     legendOffset: 32,
+//   }}
+//   axisLeft={{
+//     tickSize: 5,
+//     tickPadding: 5,
+//     tickRotation: 0,
+//     legend: "intensity",
+//     legendPosition: "middle",
+//     legendOffset: -40,
+//   }}
+//   maxValue={50} // Set the maximum value of the y-axis to 100
+//   enableLabel={false}
+//   labelSkipWidth={12}
+//   labelSkipHeight={12}
+//   labelTextColor={{
+//     from: "color",
+//     modifiers: [["darker", 1.6]],
+//   }}
+//   legends={[
+//     {
+//       dataFrom: "keys",
+//       anchor: "bottom-right",
+//       direction: "column",
+//       justify: false,
+//       translateX: 120,
+//       translateY: 0,
+//       itemsSpacing: 2,
+//       itemWidth: 100,
+//       itemHeight: 20,
+//       itemDirection: "left-to-right",
+//       itemOpacity: 0.85,
+//       symbolSize: 20,
+//       effects: [
+//         {
+//           on: "hover",
+//           style: {
+//             itemOpacity: 1,
+//           },
+//         },
+//       ],
+//     },
+//   ]}
+//   role="application"
+//   barAriaLabel={function (e) {
+//     return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
+//   }}
+// />
+
+    
     <ResponsiveBar
       data={finalRenderingData} // changed
       theme={{
